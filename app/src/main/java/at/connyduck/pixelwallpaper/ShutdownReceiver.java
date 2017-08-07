708350b99b4b48c881337f34e5986dbd0da96b3b
Conny Duck <k.pozniak@gmx.at>
2017-08-07 11:34:24 +0200

    small improvements

--- app/src/main/java/at/connyduck/pixelwallpaper/ShutdownReceiver.java
+++ app/src/main/java/at/connyduck/pixelwallpaper/ShutdownReceiver.java
@@ -3,7 +3,6 @@ package at.connyduck.pixelwallpaper;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class ShutdownReceiver extends BroadcastReceiver {
     private PixelWallpaperService.PixelWallpaperEngine pwe;
